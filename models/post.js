eac5eaca948c6733d8947c783648986952ce51ac
arvindkumar8434 <arvindkrr2003@gmail.com>
2025-09-05 21:35:55 +0530

    Create post.js

---
+++ models/post.js
@@ -0,0 +1,11 @@
+const { DataTypes } = require("sequelize");
+module.exports = (sequelize) =>
+  sequelize.define(
+    "Post",
+    {
+      id: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true },
+      title: { type: DataTypes.STRING, allowNull: false },
+      content: { type: DataTypes.TEXT, allowNull: false }
+    },
+    { timestamps: true, tableName: "posts" }
+  );

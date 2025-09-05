cee046ad2873734286d84bc3a6800a3becaf50c0
arvindkumar8434 <arvindkrr2003@gmail.com>
2025-09-05 21:35:31 +0530

    Create user.js

---
+++ models/user.js
@@ -0,0 +1,12 @@
+const { DataTypes } = require("sequelize");
+module.exports = (sequelize) =>
+  sequelize.define(
+    "User",
+    {
+      id: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true },
+      email: { type: DataTypes.STRING, unique: true, allowNull: false, validate: { isEmail: true } },
+      password: { type: DataTypes.STRING, allowNull: false },
+      name: { type: DataTypes.STRING, allowNull: true }
+    },
+    { timestamps: true, tableName: "users" }
+  );

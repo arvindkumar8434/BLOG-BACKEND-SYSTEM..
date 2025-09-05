e953125122538f5ddd2e4d7bb460da0a72f4d8c9
arvindkumar8434 <arvindkrr2003@gmail.com>
2025-09-05 21:36:27 +0530

    Create comment.js

---
+++ models/comment.js
@@ -0,0 +1,10 @@
+const { DataTypes } = require("sequelize");
+module.exports = (sequelize) =>
+  sequelize.define(
+    "Comment",
+    {
+      id: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true },
+      content: { type: DataTypes.TEXT, allowNull: false }
+    },
+    { timestamps: true, tableName: "comments" }
+  );
